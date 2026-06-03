f268fc57e7c24ff84d4a623aa48e44ac1f19882a
mahesh242 <maheshmeena6458@gmail.com>
2026-06-03 11:49:30 +0530

    style: add whitespace for improved readability in Post model definition

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean
 from sqlalchemy.sql import func
 from .database import Base
 
+
 class Post(Base):
     __tablename__ = "posts"
 
